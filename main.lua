523fa29ead20febf4d1dae85d66dea4c1f8cfd5a
cliffhangar107 <kliff.simon@gmail.com>
2013-06-02 22:45:59 -0700

    now using physics

--- main.lua
+++ main.lua
@@ -1,31 +1,74 @@
-local myTextObject = display.newText('.', 0, 0, 'Arial', 60);
-myTextObject:setTextColor( 255, 0, 0 );
+display.setStatusBar(display.HiddenStatusBar);
+local physics = require 'physics';
+physics.start();
+physics.setGravity(0,0);
 
-ySpeed = 8;
-xSpeed = 8;
-yTranslation = 1;
-xTranslation = 1;
+--Set Up Ball
+local ball = display.newImage('ball.png');
+ball.y = 30;
+physics.addBody(ball);
 
-local yTopWall = myTextObject.y;
-local yBottomWall = display.actualContentHeight - myTextObject.y;
+print(display.contentHeight);
 
-local xLeftWall = myTextObject.x;
-local xRightWall = display.actualContentWidth - myTextObject.x;
+--Set Up Walls
+local leftWall = display.newImage('sideWall.png', true);
+leftWall.x = leftWall.width;
+physics.addBody(leftWall, 'static', {bounce = 1});
 
-function move()
-	if myTextObject.y == yBottomWall then
-		yTranslation = -1;
-	elseif myTextObject.y == yTopWall then
-		yTranslation = 1;
-	end
+print(leftWall.height);
+print(leftWall.width);
 
-	if myTextObject.x == xLeftWall then
-		xTranslation = 1;
-	elseif myTextObject.x == xRightWall then
-		xTranslation = -1;
-	end
+local rightWall = display.newImage('sideWall.png', true);
+rightWall.x = display.contentWidth - rightWall.width;
+physics.addBody(rightWall, 'static', {bounce = 1});
 
-	myTextObject:translate(xSpeed * xTranslation, ySpeed * yTranslation);
-end
+local topWall = display.newImage('topWall.png', true);
+topWall.y = topWall.height;
+physics.addBody(topWall, 'static', {bounce = 1});
 
-timer.performWithDelay(1, move, -1);
+local bottomWall = display.newImage('topWall.png', true);
+bottomWall.y = display.contentHeight - bottomWall.height;
+physics.addBody(bottomWall, 'static', {bounce = 1});
+
+local xVelocity = 10;
+
+ball:setLinearVelocity(300,200);
+
+
+
+
+
+-- ySpeed = 8;
+-- xSpeed = 8;
+-- yTranslation = 1;
+-- xTranslation = 1;
+
+-- local yTopWall = ball.y;
+-- local yBottomWall = display.actualContentHeight - ball.y;
+
+-- local xLeftWall = ball.x;
+-- local xRightWall = display.actualContentWidth - ball.x;
+
+-- function move()
+-- 	if ball.y == yBottomWall then
+-- 		yTranslation = -1;
+-- 	elseif ball.y == yTopWall then
+-- 		yTranslation = 1;
+-- 	end
+
+-- 	if ball.x == xLeftWall then
+-- 		xTranslation = 1;
+-- 	elseif ball.x == xRightWall then
+-- 		xTranslation = -1;
+-- 	end
+
+-- 	ball:translate(xSpeed * xTranslation, ySpeed * yTranslation);
+-- end
+
+--timer.performWithDelay(1, move, -1);
+
+-- function blah(event)
+-- 	transition.to(ball, {time=100, x=event.x, y=event.y})
+-- end
+
+-- Runtime:addEventListener('touch', blah);
